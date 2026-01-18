1d3f3ba6dfd31b7f2b7ef934b312d6b9dab684db
Khongacom <trankhanhhuyen8506@gmail.com>
2026-01-19 01:33:35 +0700

    feat: add feature register and register UI

--- movieapp/src/main/java/com/myapp/service/AuthService.java
+++ movieapp/src/main/java/com/myapp/service/AuthService.java
@@ -1,10 +1,14 @@
 package com.myapp.service;
 
 import com.myapp.dao.UserDAO;
+import com.myapp.exception.RegisterException;
 import com.myapp.model.User;
+
+import java.util.regex.Pattern;
 
 public class AuthService {
 
+    private static final Pattern PASSWORD_PATTERN = Pattern.compile("^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$");
     private final UserDAO userDAO = new UserDAO();
 
     public boolean login(String username, String password) {
@@ -14,7 +18,31 @@ public class AuthService {
             return false;
         }
 
-        // Tạm thời so plain text (sau sẽ hash)
         return user.getPassword().equals(password);
     }
+
+    public void register(String username, String password, String confirmPassword)
+            throws RegisterException {
+
+        if (username == null || username.isBlank()
+                || password == null || confirmPassword == null) {
+            throw new RegisterException("Vui lòng nhập đầy đủ thông tin");
+        }
+
+        if (userDAO.existsByUsername(username)) {
+            throw new RegisterException("Tên đăng nhập đã tồn tại");
+        }
+
+        if (!PASSWORD_PATTERN.matcher(password).matches()) {
+            throw new RegisterException("Mật khẩu không đủ mạnh");
+        }
+
+        if (!password.equals(confirmPassword)) {
+            throw new RegisterException("Mật khẩu nhập lại không khớp");
+        }
+
+        User user = new User(username, password); // nên hash
+        userDAO.insert(user);
+    }
+
 }
